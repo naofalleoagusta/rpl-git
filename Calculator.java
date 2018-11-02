383ad69f2a87cd2f2ea7e8677b9ef71c93227dc0
NAOFAL LEO AGUSTA <i16056@ftis>
2018-11-02 13:35:57 +0700

    init

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator{
+public float plus(float a, float b){
+
+}
+
+public float minus(float a, float b){
+
+
+}
+
+public float multiply(float a, float b){
+
+
+}
+
+public float division(float a, float b){
+	
+	
+}
+
+}
