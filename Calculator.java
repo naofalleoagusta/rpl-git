ae8568e16d6581d0629afb774a9d5729579ca420
MICHAEL NATHANIEL CHRISTIAN <i16009@ftis>
2018-11-02 14:19:54 +0700

    multiply division

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,20 @@
-public class Calculator{
-public float plus(float a, float b){
 
-}
+public class Calculator {
 
-public float minus(float a, float b){
+    public float plus(float a, float b) {
 
+    }
 
-}
+    public float minus(float a, float b) {
 
-public float multiply(float a, float b){
+    }
 
+    public float multiply(float a, float b) {
+        return a*b;
+    }
 
-}
-
-public float division(float a, float b){
-	
-	
-}
+    public float division(float a, float b) {
+        return a/b;
+    }
 
 }
